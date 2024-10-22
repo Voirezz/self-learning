589397fe8e4f8c5e6530c2ee8070be171362457a
Raoul Haba <rhaba91@gmail.com>
2024-10-22 09:38:23 +0100

    completed toDoList

--- arrays/src/toDoList.java
+++ arrays/src/toDoList.java
@@ -2,41 +2,87 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class toDoList {
-    public static void main(String [] args) {
-        ArrayList<String> toDoList = new ArrayList<String>();
+    public static final Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        ArrayList<String> toDoList = new ArrayList<>();
         menu(toDoList);
     }
-    public static String menu(ArrayList<String> toDoList){
-        Scanner input = new Scanner(System.in);
 
-        System.out.print("Welcome to your very own dynamic To Do List! \n");
-        System.out.print("Please select an option: \n");
-        System.out.print("\n x: Add a task \n y: View tasks \n z: Remove a task \n");
-        String b = input.next();
+    public static void menu(ArrayList<String> toDoList) {
+        while (true) {
+            System.out.print("\n x: Add a task \n y: View tasks \n z: Remove a task \n q: Quit \n");
+            String b = input.nextLine().toLowerCase();
 
-
-        if(b.toLowerCase().equals("x")) addTask(toDoList);
-        else if(b.toLowerCase().equals("y")) viewTask(toDoList);
-        else if(b.toLowerCase().equals("z")) removeTask(toDoList);
-
-        return menu(toDoList);
+            switch (b) {
+                case "x":
+                    addTask(toDoList);
+                    break;
+                case "y":
+                    viewTask(toDoList);
+                    break;
+                case "z":
+                    removeTask(toDoList);
+                    break;
+                case "q":
+                    System.out.println("Exiting...");
+                    return;
+                default:
+                    System.out.println("Invalid option. Please try again.");
+            }
+        }
     }
+
     public static void addTask(ArrayList<String> toDoList) {
+        System.out.println("\n Add a task: ");
+        String task = input.nextLine();
+        toDoList.add(task);
 
+        System.out.println("Task added. Would you like to add another task? (y/n)");
+        String response = input.nextLine().toLowerCase();
+        if (response.equals("y")) {
+            addTask(toDoList);
+        }
     }
+
     public static void viewTask(ArrayList<String> toDoList) {
-    System.out.println("Here are all the tasks you must complete.");
-
-    for(String c : toDoList) {
-        System.out.print(c);
-
-    }
-    menu(toDoList);
-
+        if (toDoList.isEmpty()) {
+            System.out.println("You have either completed all your tasks or you've added nothing!");
+        } else {
+            System.out.println("Here are all your tasks you must complete:");
+            for (int i = 0; i < toDoList.size(); i++) {
+                System.out.println((i + 1) + ". " + toDoList.get(i));
+            }
+        }
     }
 
     public static void removeTask(ArrayList<String> toDoList) {
+        if (toDoList.isEmpty()) {
+            System.out.println("No tasks to remove.");
+            return;
+        }
 
+        System.out.println("Which number would you like to remove?");
+        for (int i = 0; i < toDoList.size(); i++) {
+            System.out.println((i + 1) + ". " + toDoList.get(i));
+        }
+
+        int index = input.nextInt();
+        input.nextLine(); // Consume newline
+
+        if (index > 0 && index <= toDoList.size()) {
+            toDoList.remove(index - 1);
+            System.out.println("Task removed. Here is your new list:");
+            viewTask(toDoList);
+        } else {
+            System.out.println("Invalid task number.");
+        }
+
+        System.out.println("Would you like to remove another task? (y/n)");
+        String response = input.nextLine().toLowerCase();
+        if (response.equals("y")) {
+            removeTask(toDoList);
+        }
     }
 }
 
