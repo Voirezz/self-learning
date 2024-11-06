115abc2bdb0a143a7cc75d655fd48580341c47a7
Raoul Haba <rhaba91@gmail.com>
2024-11-06 10:43:37 +0000

    com

---
+++ arrays/src/MusicPlaylistManager.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+public class MusicPlaylistManager { blic static void main(String[] args) {
+        ArrayList<String> playlist = new ArrayList<>();
+
+
+
+
+    }
+}
+
+
--- arrays/src/ToDoList.java
+++ arrays/src/ToDoList.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class toDoList {
+public class ToDoList {
     public static final Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
