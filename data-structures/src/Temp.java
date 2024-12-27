043ac328595f563647213a432b486e1c4890a608
Raoul <rhaba91@gmail.com>
2024-12-27 17:01:29 +0000

    binary search modifications

--- data-structures/src/Temp.java
+++ data-structures/src/Temp.java
@@ -2,37 +2,34 @@ import javax.xml.crypto.dsig.spec.HMACParameterSpec;
 
 public class Temp {
     public static void main(String [] args){
-        int start = 20;
-        int end = 50;
-        System.out.println(sophieGermain(start,end));
+     int [] arr = new int[]{1,2,3,4,5,6,7,8,9,10};
+     int target = 3;
 
+     if(binarySearch(arr, target) == -1){
+         System.out.println("Number not found");
+     }
+     else {
+         System.out.println("Target Found : " + binarySearch(arr,target));
+     }
     }
 
-    public static int sophieGermain(int start, int end) {
-        int maxCheck = 2 * end + 1;
-        boolean[] isPrime = new boolean[maxCheck + 1];
+    public static int binarySearch(int [] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
 
-        for(int i = 2; i < maxCheck; i++){
-            isPrime[i] = true;
-        }
+        while(left <= right){
+           int mid = left + (right - left) / 2;
 
-        //Sieve of Erathosthenes
-        for (int i = 2; i <= Math.sqrt(maxCheck); i++) {
-            if (isPrime[i]) {
-                for (int k = i * i; k <= maxCheck; k += i) {
-                    isPrime[k] = false;
-                }
-            }
-        }
-
-        int sum = 0;
-
-        //checker
-        for (int i = start; i <= end; i++) {
-            if (isPrime[i] && isPrime[2 * i + 1]) {
-                sum += i;
-
-            }
-        }return sum;
+           if(arr[mid] == target){
+               return target;
+           }
+           if(arr[mid] < target) {
+               left = mid + 1;
+           }
+           else {
+               right = mid - 1;
+           }
+       }
+        return -1;
     }
 }
