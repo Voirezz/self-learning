00c1e126e68140c2071a662a5ea83bc551f4352d
Raoul <rhaba91@gmail.com>
2024-12-27 14:55:06 +0000

    Sophie Germain Implementation

--- data-structures/src/SieveOfErathosthenes.java
+++ data-structures/src/SieveOfErathosthenes.java
@@ -2,18 +2,19 @@ public class SieveOfErathosthenes {
     public static void main(String [] args){
         int n = 25;
 
+        //Making a boolean array and setting everything after 1 as true
         boolean[] isPrime = new boolean[n];
         isPrime[0] = false;
-
         for(int i = 1; i < n; i++){
             isPrime[i] = true;
         }
 
+        //Check if it's a prime number, if it is prints it out
         for(int i = 2; i <= n; i++){
             if(isPrime[i-1]) {
                 System.out.print(i + " ");
 
-
+                //Marks the multiples of the prime number as not prime.
                 for (int j = i * i; j <= n; j += i) {
                     isPrime[j - 1] = false;
                 }
--- data-structures/src/Temp.java
+++ data-structures/src/Temp.java
@@ -1,5 +1,38 @@
+import javax.xml.crypto.dsig.spec.HMACParameterSpec;
+
 public class Temp {
     public static void main(String [] args){
+        int start = 20;
+        int end = 50;
+        System.out.println(sophieGermain(start,end));
 
+    }
+
+    public static int sophieGermain(int start, int end) {
+        int maxCheck = 2 * end + 1;
+        boolean[] isPrime = new boolean[maxCheck + 1];
+
+        for(int i = 2; i < maxCheck; i++){
+            isPrime[i] = true;
+        }
+
+        //Sieve of Erathosthenes
+        for (int i = 2; i <= Math.sqrt(maxCheck); i++) {
+            if (isPrime[i]) {
+                for (int k = i * i; k <= maxCheck; k += i) {
+                    isPrime[k] = false;
+                }
+            }
+        }
+
+        int sum = 0;
+
+        //checker
+        for (int i = start; i <= end; i++) {
+            if (isPrime[i] && isPrime[2 * i + 1]) {
+                sum += i;
+
+            }
+        }return sum;
     }
 }
